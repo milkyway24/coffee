24e2920ac0650a1556a1b9425e349f057eb94c3d
milkyway24 <79013716+milkyway24@users.noreply.github.com>
2025-05-15 01:30:54 +0300

    Add files via upload

---
+++ js/db.js
@@ -0,0 +1,237 @@
+export const stations = [
+    {
+        name: 'Авиамоторная',
+        number: '2707',
+        place: 'https://yandex.ru/maps/-/CHrtb-9g',
+        // drive: '#',
+        // parking: '#',
+        // descr: 'Взять ключ от конструкции. Заезжать под кирпич, только посмотреть или в округе нет ДПС. Дальше мимо шлагбаума слева по тротуару.'
+    },
+    {
+        name: 'Авиамоторная',
+        number: '2774',
+        place: 'https://yandex.ru/maps/-/CHrtfRjC'
+    },
+    {
+        name: 'Авиамоторная',
+        number: '2439',
+        place: 'https://yandex.ru/maps/-/CHrtfG1K',
+    },
+    {
+        name: 'Автозаводская',
+        number: '2421',
+        place: 'https://yandex.ru/maps/-/CHBxzYnb'
+    },
+    {
+        name: 'Автозаводская',
+        number: '3516',
+        place: 'https://yandex.ru/maps/-/CHrlBM4r'
+    },
+    {
+        name: 'Академика Янгеля',
+        number: '2733',
+        place: 'https://yandex.ru/navi/-/CHrd7Szi'
+    },
+    {
+        name: 'Академика Янгеля',
+        number: '2215',
+        place: 'https://yandex.ru/navi/-/CHrd7HNz'
+    },
+    {
+        name: 'Академика Янгеля',
+        number: '2764',
+        place: 'https://yandex.ru/navi/-/CHrd7XMK'
+    },
+    {
+        name: 'Академическая',
+        number: '2200',
+        place: 'https://yandex.ru/maps/-/CHvIa81q'
+    },
+    {
+        name: 'Академическая',
+        number: '2205',
+        place: 'https://yandex.ru/maps/-/CHvIeM8C'
+    },
+    {
+        name: 'Александровский сад',
+        number: '2422',
+    },
+    {
+        name: 'Алтуфьево',
+        number: '2157',
+        place: 'https://yandex.ru/maps/-/CHr-jOoA',
+        drive: 'https://yandex.ru/maps/-/CHr-jWPx',
+        descr: 'Есть возможность так же подъехать с другой стороны Алтуфьевского шоссе'
+    },
+    {
+        name: 'Алтуфьево',
+        number: '2158',
+        place: 'https://yandex.ru/maps/-/CHr-j2I0',
+        parking: 'https://yandex.ru/maps/-/CHvFIRJl',
+        descr: 'Выход 7. Рядом есть бесплатная парковка, либо на тротуар с пешеходника на светофоре'
+    },
+    {
+        name: 'Алтуфьево',
+        number: '2768',
+        place: 'https://yandex.ru/maps/-/CHr-j2I0',
+        parking: 'https://yandex.ru/maps/-/CHvFIRJl',
+        descr: 'Стоит в одном переходе с 2158'
+    },
+    {
+        name: 'Аминьевская',
+        number: '2777',
+    },
+    {
+        name: 'Аминьевская',
+        number: '3513',
+    },
+    {
+        name: 'Аминьевская',
+        number: '2418',
+    },
+    {
+        name: 'Аминьевское депо',
+        number: '2282',
+    },
+    {
+        name: 'Аминьевское депо',
+        number: '2283',
+    },
+    {
+        name: 'Аминьевское депо',
+        number: '2280',
+    },
+    {
+        name: 'Аминьевское депо',
+        number: '2281',
+    },
+    {
+        name: 'Аннино',
+        number: '2491',
+        place: 'https://yandex.ru/maps/-/CHrdq89o',
+        descr: 'Закрывается в 2:00'
+    },
+    {
+        name: 'Арбатская',
+        number: '2411',
+    },
+    {
+        name: 'Аэропорт',
+        number: '2758',
+        place: 'https://yandex.ru/maps/-/CHvMjRzT',
+        descr: 'Закрывается в 2:00'
+    },
+    {
+        name: 'Бабушкинская',
+        number: '2490',
+        place: 'https://yandex.ru/navi/-/CHfd4QYs',
+    },
+    {
+        name: 'Бабушкинская',
+        number: '2763',
+        place: 'https://yandex.ru/navi/-/CHfda4kL',
+    },
+    {
+        name: 'Баррикадная',
+        number: '2770',
+    },
+    {
+        name: 'Беговая',
+        number: '2171',
+    },
+    {
+        name: 'Беговая',
+        number: '2172',
+    },
+    {
+        name: 'Беломорская',
+        number: '2296',
+        place: 'https://yandex.ru/maps/-/CHvMj86J',
+        descr: 'Закрывается в 2:00'
+    },
+    {
+        name: 'Белорусская',
+        number: '2795',
+        place: 'https://yandex.ru/navi/-/CHfdiE-a',
+        parking: 'https://yandex.ru/navi/-/CHfdiU0D',
+        descr: 'Закрывается в 2:00. Рядом есть платная парковка'
+    },
+    {
+        name: 'Беляево',
+        number: '2209',
+        place: 'https://yandex.ru/maps/-/CHvIeQ3d',
+    },
+    {
+        name: 'Беляево',
+        number: '2755',
+        place: 'https://yandex.ru/maps/-/CHvFMV2n',
+    },
+    {
+        name: 'Беляево',
+        number: '2208',
+        place: 'https://yandex.ru/maps/-/CHvIeYo5',
+    },
+    {
+        name: 'Бибирево',
+        number: '2469',
+        place: 'https://yandex.ru/maps/-/CHr-nC7Z',
+        descr: 'В дневное время рядом есть бесплатная парковка торгового центра. Ночью можно парковаться на тротуаре'
+    },
+    {
+        name: 'Бибирево',
+        number: '2233',
+        place: 'https://yandex.ru/maps/-/CHr-n89Z',
+        drive: 'https://yandex.ru/maps/-/CHr-nP1B',
+        descr: 'Заезжать через заправку'
+    },
+    {
+        name: 'Библиотека имени Ленина',
+        number: '2766',
+    },
+    {
+        name: 'Борисово',
+        number: '2260',
+        place: 'https://yandex.ru/navi/-/CHrhqLMj',
+    },
+    {
+        name: 'Боровское шоссе',
+        number: '2722',
+    },
+    {
+        name: 'Ботанический сад',
+        number: '2729',
+        place: 'https://yandex.ru/navi/-/CHfdNB3j',
+        drive: 'https://yandex.ru/navi/-/CHfdNKNd',
+        descr: 'Закрывается в 2:00'
+    },
+    {
+        name: 'Братеево депо',
+        number: '2203',
+        place: 'https://yandex.ru/maps/-/CHrluJOv',
+    },
+    {
+        name: 'Братеево депо',
+        number: '2241',
+        place: 'https://yandex.ru/maps/-/CHrlUD6Z',
+    },
+    {
+        name: 'Братиславская',
+        number: '2202',
+        place: 'https://yandex.ru/maps/-/CHrlU0K1',
+    },
+    {
+        name: 'Братиславская',
+        number: '2429',
+        place: 'https://yandex.ru/maps/-/CHrlUD6Z',
+    },
+    {
+        name: 'Бутырская',
+        number: '2298',
+        place: 'https://yandex.ru/navi/-/CHfdND6-',
+    },
+    {
+        name: 'Бутырская',
+        number: '2299',
+        place: 'https://yandex.ru/navi/-/CHfdRI3o',
+    },
+]
---
+++ js/index.js
@@ -0,0 +1,119 @@
+import { stations } from "./db.js";
+
+const inpStation = document.querySelector('.js-station');
+const inpNum = document.querySelector('.js-number');
+const places = document.querySelector('.js-places');
+const loader = document.querySelector('.js-loader');
+const notFound = document.querySelector('.js-not-found');
+
+function findStations(stat, num) {
+    const findedStArr = stations.filter((el) => {
+
+        if (stat && num) {
+            return el.name.toLowerCase().includes(stat.toLowerCase()) && el.number.toLowerCase().includes(num.toLowerCase());
+        }
+
+        if (!num) {
+            return el.name.toLowerCase().includes(stat.toLowerCase());
+        }
+
+        if (!stat) {
+            return el.number.toLowerCase().includes(num.toLowerCase());
+        }
+
+    });
+
+    return findedStArr
+}
+
+function cleanPlaces() {
+    places.textContent = '';
+}
+
+function getPlaceTemplate() {
+    const temp = document.querySelector('.js-template');
+    const placeTemp = temp.cloneNode(true);
+    return placeTemp.content;
+}
+
+function preparePlace(placeObj) {
+    const placeTemp = getPlaceTemplate();
+    const title = placeTemp.querySelector('.js-place-title');
+    const num = placeTemp.querySelector('.js-place-num');
+    const enter = placeTemp.querySelector('.js-place-enter');
+    const drive = placeTemp.querySelector('.js-place-drive');
+    const parking = placeTemp.querySelector('.js-place-parking');
+    const descr = placeTemp.querySelector('.js-place-descr');
+
+    title.textContent = placeObj.name;
+    num.textContent = placeObj.number;
+
+    if (placeObj.place) {
+        const link = enter.querySelector('.js-place-link');
+        link.href = `${placeObj.place}`;
+    }
+
+    if (placeObj.drive) {
+        const link = drive.querySelector('.js-place-link');
+        link.href = placeObj.drive;
+    } else {
+        drive.remove();
+    }
+
+    if (placeObj.parking) {
+        const link = parking.querySelector('.js-place-link');
+        link.href = placeObj.parking;
+    } else {
+        parking.remove();
+    }
+
+    if (!placeObj.place && !placeObj.drive && !placeObj.parking && !placeObj.descr) {
+        const text = descr.querySelector('.js-place-descr-text');
+        text.textContent = 'Точка находится на уточнении';
+        enter.remove();
+    } else if (placeObj.descr) {
+        const text = descr.querySelector('.js-place-descr-text');
+        text.textContent = placeObj.descr;
+    } else {
+        descr.remove();
+    }
+
+    return placeTemp;
+}
+
+function printPlaces(placesArr) {
+    cleanPlaces();
+
+    placesArr.forEach((el) => {
+        const placeEl = preparePlace(el);
+        places.append(placeEl);
+    });
+}
+
+function onInpPrint() {
+    cleanPlaces();
+    loader.style.display = 'block';
+
+    setTimeout(() => {
+        if ((!inpStation.value.trim() && !inpNum.value.trim())) {
+            cleanPlaces();
+            loader.style.display = 'none';
+            notFound.style.display = 'none';
+            return;
+        }
+    
+        const stationsArr = findStations(inpStation.value.trim(), inpNum.value.trim());
+    
+        if (stationsArr.length) {
+            notFound.style.display = 'none';
+            printPlaces(stationsArr);
+        } else {
+            notFound.style.display = 'block';
+        }
+
+        loader.style.display = 'none';
+    }, 500);
+}
+
+inpStation.addEventListener('input', onInpPrint);
+inpNum.addEventListener('input', onInpPrint);
