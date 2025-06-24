7aa7c27db3674437e965c388d7cf27d571ecdd3c
milkyway24 <79013716+milkyway24@users.noreply.github.com>
2025-06-24 19:09:17 +0300

    Update auto-script.js

--- auto-script.js
+++ auto-script.js
@@ -1,4 +1,4 @@
-// Версия 24.06.2025 19:05
+// Версия 24.06.2025 19:10
 
 const currentUrl = window.location.href;
 const substringToCheck = "rtvmcloading_m";
@@ -345,7 +345,7 @@ if (currentUrl.includes(substringToCheck)) {
 
 
         btn.addEventListener('click', function () {
-            navigator.vibrate(100);
+            window.navigator.vibrate(100);
             writeTele(inpRows);
             btn.remove();
         });
